d0a9679890e19e789e09b1913fe397c435dc84e0
cigneve <chaos435435@gmail.com>
2024-05-28 12:24:54 +0300

    disable specific hardware settings until tuning

--- hosts/babadir/hardware.nix
+++ hosts/babadir/hardware.nix
@@ -18,23 +18,24 @@
   # during LUKS.
 
   # upstream includes SATA drivers etc. which we don't build into the kernel.
-  boot.initrd.includeDefaultModules = false;
-  boot.initrd.availableKernelModules = lib.mkForce [
-    "xhci_pci"
-    "nvme"
-    "sd_mod"
-    "dm_mod"
-    "dm_crypt"
-    "cryptd"
-    # required for keyboard support at init
-    # "intel_lpss"
-    # "intel_lpss_pci"
-    "8250_dw"
-    "surface_aggregator"
-    "surface_aggregator_registry"
-    "surface_hid_core"
-    "surface_hid"
-  ];
+
+  # boot.initrd.includeDefaultModules = false;
+  # boot.initrd.availableKernelModules = lib.mkForce [
+  #   "xhci_pci"
+  #   "nvme"
+  #   "sd_mod"
+  #   "dm_mod"
+  #   "dm_crypt"
+  #   "cryptd"
+  #   # required for keyboard support at init
+  #   # "intel_lpss"
+  #   # "intel_lpss_pci"
+  #   "8250_dw"
+  #   "surface_aggregator"
+  #   "surface_aggregator_registry"
+  #   "surface_hid_core"
+  #   "surface_hid"
+  # ];
 
   boot.kernelModules = ["kvm-amd"];
 

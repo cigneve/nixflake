6f48ba94fe9eb5f96a060cb3b0620ca0ed9020db
Yusuf Said Aktan <contact@aktan.org>
2024-06-21 20:55:39 +0300

    fix foot theme

--- profiles/develop/foot/default.nix
+++ profiles/develop/foot/default.nix
@@ -1,25 +1,25 @@
 {pkgs,
 lib,...}:
 let
-  revolver = "#281733";
-  berry = "#3A2A4D";
-  berry_fade = "#5A3D6E";
-  berry_dim = "#47345E";
-  berry_saturated = "#2B1C3D";
-  berry_desaturated = "#886C9C";
-  bubblegum = "#D678B5";
-  gold = "#E3C0A8";
-  lilac = "#C7B8E0";
-  mint = "#7FC9AB";
-  violet = "#C78DFC";
-  almond = "#ECCDBA";
+  revolver = "281733";
+  berry = "3A2A4D";
+  berry_fade = "5A3D6E";
+  berry_dim = "47345E";
+  berry_saturated = "2B1C3D";
+  berry_desaturated = "886C9C";
+  bubblegum = "D678B5";
+  gold = "E3C0A8";
+  lilac = "C7B8E0";
+  mint = "7FC9AB";
+  violet = "C78DFC";
+  almond = "ECCDBA";
 
 in {
   
-
+  programs.foot.enable = true;
   programs.foot.settings = {
     colors = {
-      foreground = gold;
+      foreground = lilac;
       background = revolver;
       regular0=berry_saturated;
       regular1=bubblegum;

f42a3e724645f7083a0c96bc3e3c75cd1b5e295c
Yusuf Said Aktan <contact@aktan.org>
2024-06-21 12:20:41 +0300

    finish setting interception

--- profiles/laptop/default.nix
+++ profiles/laptop/default.nix
@@ -33,7 +33,7 @@
       - JOB: "${pkgs.interception-tools}/bin/intercept -g $DEVNODE | ${pkgs.interception-tools-plugins.dual-function-keys}/bin/dual-function-keys -c /etc/dual-function-keys.yaml | ${pkgs.interception-tools}/bin/uinput -d $DEVNODE"
         DEVICE:
           EVENTS:
-            EV_KEY: [KEY_A, KEY_CAPSLOCK]
+            EV_KEY: [KEY_CAPSLOCK]
     '';
   };
 

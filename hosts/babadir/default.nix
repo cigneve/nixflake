906502f452caaa6224a6e4a83d317742be617a4e
Yusuf Said Aktan <contact@aktan.org>
2024-06-21 11:59:53 +0300

    remove redundant comments

--- hosts/babadir/default.nix
+++ hosts/babadir/default.nix
@@ -15,13 +15,7 @@
 in {
   imports = [
     ./hardware.nix
-    # Take an empty *readonly* snapshot of the root subvolume,
-    # which we'll eventually rollback to on every boot.
-    # sudo mount /dev/mapper/cryptroot -o subvol=root /mnt/root
-    # btrfs subvolume snapshot -r /mnt/root /mnt/root-blank
-
     # sudo nix run github:nix-community/disko --extra-experimental-features flakes --extra=experimental-features nix-command -- --mode disko --flake github:archseer/snowflake#trantor
-    # --arg disks ["/dev/nvme0p1"] not necessary?
     ./disko.nix
     ../../profiles/zram # Use zram for swap
     ../../profiles/laptop

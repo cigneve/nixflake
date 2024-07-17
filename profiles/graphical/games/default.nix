bc135d044e7675f3c0dba249daa06ac508b076be
Yusuf Said Aktan <contact@aktan.org>
2024-07-17 10:56:26 +0300

    graphical module remove steam

--- profiles/graphical/games/default.nix
+++ profiles/graphical/games/default.nix
@@ -1,11 +1,6 @@
 {pkgs, ...}: {
   imports = [./udev.nix];
 
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-  };
 
   # hardware.opengl.driSupport32Bit = true;
   hardware.graphics.enable = true;

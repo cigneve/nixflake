c7cde33400784aee6a8b795e0ba5f333694eac56
Yusuf Said Aktan <contact@aktan.org>
2024-07-22 15:53:13 +0300

    zram config enforce high vm.swappiness setting

--- profiles/zram/default.nix
+++ profiles/zram/default.nix
@@ -1,9 +1,9 @@
-{
+{lib,...}: {
   # Use zram for swap
   swapDevices = [];
   zramSwap.enable = true;
   # zram is relatively cheap, prefer swap
-  boot.kernel.sysctl."vm.swappiness" = 180;
+  boot.kernel.sysctl."vm.swappiness" = lib.mkDefault 180;
   boot.kernel.sysctl."vm.watermark_boost_factor" = 0;
   boot.kernel.sysctl."vm.watermark_scale_factor" = 125;
   # zram is in memory, no need to readahead

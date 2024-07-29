8059f12abce1da326caa5f54672fbd102f306361
Yusuf Said Aktan <contact@aktan.org>
2024-07-29 22:10:57 +0300

    fish config add zelij integration (home-manager integration is overriden by our config)

--- profiles/develop/fish/config.fish
+++ profiles/develop/fish/config.fish
@@ -1,5 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    type -q zellij && eval (zellij setup --generate-auto-start fish | string collect)
 end
 
 fish_add_path $HOME/go/bin /usr/local/sbin

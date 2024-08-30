296868fe39a7e63a8d561745ab67251e6a0a66cb
Yusuf Said Aktan <contact@ysaktan.com>
2024-08-30 14:47:59 +0300

    helix config add languages.toml

--- profiles/develop/helix/default.nix
+++ profiles/develop/helix/default.nix
@@ -6,4 +6,5 @@
   programs.helix.enable = true;
   xdg.configFile."helix/themes".source = ./themes;
   xdg.configFile."helix/config.toml".source = ./config.toml;
+  xdg.configFile."helix/languages.toml".source = ./languages.toml;
 }

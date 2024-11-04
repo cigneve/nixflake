e3e9de6f6c725bd4c74fd0b229624796fd3d15de
Yusuf Said Aktan <contact@ysaktan.com>
2024-11-04 18:17:22 +0300

    obs plugins

--- profiles/studio/video.nix
+++ profiles/studio/video.nix
@@ -1,12 +1,14 @@
 {pkgs, ...}: {
   environment.systemPackages = with pkgs; [
     kdenlive
+    obs-cli
     (pkgs.wrapOBS {
         plugins = with pkgs.obs-studio-plugins; [
           wlrobs
           obs-backgroundremoval
           obs-pipewire-audio-capture
           obs-source-record
+          obs-websocket
         ];
       })
     #davinci-resolve-studio

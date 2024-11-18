c08ae938146b9ffddd8925bd98c1e4809def7b5a
Yusuf Said Aktan <contact@ysaktan.com>
2024-11-18 17:33:42 +0300

    feat(yazi): update config

--- profiles/develop/yazi/config/init.lua
+++ profiles/develop/yazi/config/init.lua
@@ -1,1 +1,11 @@
+local git_theme = {
+  modified_sign = "MOD",
+  added_sign = "ADD",
+  untracked_sign = "UT",
+  ignored_sign = "IGN",
+  deleted_sign = "DEL",
+  updated_sign = "UPD",
+}
+
+THEME.git = git_theme
 pcall(function() require("git"):setup() end)

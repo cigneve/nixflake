e0c59f1a2f8c358c98c23fc566db438e6be90bc6
Yusuf Said Aktan <contact@ysaktan.com>
2025-03-04 10:18:36 +0300

    feat(wsl): update profile

--- hosts/wsl/default.nix
+++ hosts/wsl/default.nix
@@ -5,50 +5,14 @@
 }: let
 in {
   imports = [
-    # ./hardware.nix
-    # Take an empty *readonly* snapshot of the root subvolume,
-    # which we'll eventually rollback to on every boot.
-    # sudo mount /dev/mapper/cryptroot -o subvol=root /mnt/root
-    # btrfs subvolume snapshot -r /mnt/root /mnt/root-blank
-
-    # sudo nix run github:nix-community/disko --extra-experimental-features flakes --extra=experimental-features nix-command -- --mode disko --flake github:archseer/snowflake#trantor
-    # --arg disks ["/dev/nvme0p1"] not necessary?
-    # ./disko.nix
-    # ../../profiles/zram # Use zram for swap
-    # ../../profiles/laptop
-    # ../../profiles/network # sets up wireless
-    ../../profiles/graphical
     ../../profiles/develop
-    # ../../profiles/misc/yubikey.nix
     ../../users/baba
     ../../users/root
   ];
 
   networking.firewall.enable = lib.mkForce false;
 
-  # boot.loader.systemd-boot.enable = true;
-  # boot.loader.systemd-boot.editor = false;
-
-  # boot.loader.efi.canTouchEfiVariables = true;
-
-  # btrfs
-  # boot.initrd.supportedFilesystems = ["btrfs"];
-  # services.btrfs.autoScrub.enable = true;
-
-  # Disk
-  ## We imported the needed disk configuration from disko.nix
-
-  # hardware = {
-  #   enableAllFirmware = true;
-  #   firmware = [pkgs.wireless-regdb];
-  # };
-
-  # nix.maxJobs = lib.mkDefault 8;
-  # nix.systemFeatures = [ "gccarch-haswell" ];
-
-  # Track list of enabled modules for localmodconfig generation.
-  # environment.systemPackages = [pkgs.modprobed-db
-  #  pkgs.compsize];
+  nix.maxJobs = lib.mkDefault 8;
 
   wsl = {
     enable = true;

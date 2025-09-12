cb1deaaf775a99cd5f08ae18e4711975b1a2eec4
Yusuf Said Aktan <contact@aktan.org>
2025-09-13 00:27:34 +0300

    vscode: add profiles

--- profiles/develop/vscode.nix
+++ profiles/develop/vscode.nix
@@ -1,11 +1,35 @@
 {lib,pkgs,config,...}:let cfg = config.vscode_module; in {
   options = {
-    vscode_module.enable = lib.mkEnableOption "Enable personal vscode module";
+    vscode_module.enable = lib.mkEnableOption "personal vscode module";
   };
 
   config = lib.mkIf cfg.enable {
     programs.vscode = {
       enable = true;
+      profiles = {
+        default = {
+          enableUpdateCheck = false;
+          extensions = with pkgs.vscode-marketplace; [
+            gregoire.dance
+            mhutchie.git-graph
+            usernamehw.errorlens
+            mkhl.direnv
+            jnoortheen.nix-ide
+            arrterian.nix-env-selector
+          ];
+        };
+        rust = {
+          extensions = with pkgs.vscode-marketplace; [
+            rust-lang.rust-analyzer
+          ];
+        };
+        python = {
+          extensions = with pkgs.vscode-marketplace; [
+            ms-python.python
+
+          ];
+        };
+      };
     };
   };
 }

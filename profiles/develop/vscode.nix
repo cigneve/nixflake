06f6e9df85493835d98c575611983266402bb168
Yusuf Said Aktan <contact@aktan.org>
2025-09-24 14:08:50 +0300

    vscode: edit profiles

--- profiles/develop/vscode.nix
+++ profiles/develop/vscode.nix
@@ -1,4 +1,11 @@
-{lib,pkgs,config,...}:let cfg = config.vscode_module; in {
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}: let
+  cfg = config.vscode_module;
+in {
   options = {
     vscode_module.enable = lib.mkEnableOption "personal vscode module";
   };
@@ -6,28 +13,46 @@
   config = lib.mkIf cfg.enable {
     programs.vscode = {
       enable = true;
-      profiles = {
+      profiles = let
+        default_extensions = with pkgs.vscode-marketplace; [
+          gregoire.dance
+          gregoire.dance-helix
+          mhutchie.git-graph
+          usernamehw.errorlens
+          mkhl.direnv
+          jnoortheen.nix-ide
+          arrterian.nix-env-selector
+          quicktype.quicktype          
+          robbowen.synthwave-vscode
+          myriad-dreamin.tinymist
+        ];
+      in {
         default = {
           enableUpdateCheck = false;
-          extensions = with pkgs.vscode-marketplace; [
-            gregoire.dance
-            mhutchie.git-graph
-            usernamehw.errorlens
-            mkhl.direnv
-            jnoortheen.nix-ide
-            arrterian.nix-env-selector
-          ];
+          extensions = default_extensions;
         };
         rust = {
           extensions = with pkgs.vscode-marketplace; [
             rust-lang.rust-analyzer
-          ];
+          ] ++ default_extensions;
         };
         python = {
           extensions = with pkgs.vscode-marketplace; [
             ms-python.python
-
-          ];
+            ms-python.debugpy
+            ms-python.black-formatter
+            ms-python.vscode-python-envs
+          ] ++ default_extensions;
+        };
+        go = {
+          extensions = with pkgs.vscode-marketplace; [
+            golang.go
+          ] ++ default_extensions;
+        };
+        beancount = {
+          extensions = with pkgs.vscode-marketplace; [
+            lencerf.beancount
+          ] ++ default_extensions;
         };
       };
     };

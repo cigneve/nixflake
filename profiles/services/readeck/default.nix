fbfe27480a65ac14a41624f6d579bf1a1fb03083
Yusuf Said Aktan <contact@aktan.org>
2026-02-03 12:46:13 +0300

    readeck: add quadlet

--- profiles/services/readeck/default.nix
+++ profiles/services/readeck/default.nix
@@ -1,28 +1,37 @@
-{lib,config,...}: let cfg = config.cigneve_readeck; in {
-  home-manager.users.services = {
-    virtualisation.quadlet = {
-      containers.readeck = {
-        autoStart = true;
-        containerConfig = {
-          image = "codeberg.org/readeck/readeck:latest";
-          PublishPort = ["8000:8000"];
+{
+  lib,
+  config,
+  ...
+}: let
+  cfg = config.cigneve_readeck;
+in {
+  options = {
+    cigneve_readeck.enable = lib.mkEnableOption "Enable custom readeck podman service module";
+  };
+  config = lib.mkIf cfg.enable{
+    home-manager.users.services = {
+      virtualisation.quadlet = {
+        containers.readeck = {
+          autoStart = true;
           containerConfig = {
-            Image = "codeberg.org/readeck/readeck:latest";
+            image = "codeberg.org/readeck/readeck:latest";
 
             publishPorts = ["8080:8080"];
             # Environment variables for custom ports or logs
-            Environment = [
+            # Main data volume (SQLite DB + Archived articles)
+            volumes = [
+              "/var/lib/readeck:/readeck:Z"
+            ];
+          };
+          serviceConfig = {
+            environment = [
               "READECK_SERVER_HOST=0.0.0.0"
               "READECK_SERVER_PORT=8000"
               "READECK_LOG_LEVEL=info"
-            ];
-            # Main data volume (SQLite DB + Archived articles)
-            Volume = [
-              "/var/lib/readeck:/readeck:Z"
             ];
           };
         };
       };
     };
-  };  
+  };
 }

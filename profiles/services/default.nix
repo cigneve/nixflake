bd8cf2009d1d4ab3e0697c1c74fd7b38552b1b3e
Yusuf Said Aktan <contact@aktan.org>
2026-02-03 12:46:13 +0300

    services: fix home-manager

--- profiles/services/default.nix
+++ profiles/services/default.nix
@@ -2,13 +2,14 @@
   imports = [./readeck ./mealie ./stalwart ./karakeep];
   config =  {
     users.users.services = {
-      isSystemUser = true;
+      isNormalUser = true;
       group = "services_group";
-      extraGroups = ["docker" "podman"];
+      extraGroups = ["docker" "podman" "wheel"];
       uid = 1001; # Matches the UID in .env
       linger = true;
       autoSubUidGidRange = true;
     };
     users.groups.services_group.gid = 1001;
+    home-manager.users.services.home.stateVersion = "24.05";
   };
 }

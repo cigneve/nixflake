43ff568b19c967baffd2df5bbd0a3d5720ec56a2
Yusuf Said Aktan <contact@aktan.org>
2026-03-09 23:02:21 +0300

    secrets

--- hosts/vm/sops.nix
+++ hosts/vm/sops.nix
@@ -1,5 +1,6 @@
 {
   sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-  sops.defaultSopsFile = ../../secrets/secrets.yaml;
-  sops.secrets."api-key" = {};
+  sops.defaultSopsFile = ./secrets/secrets.yaml;
+  sops.defaultSopsFormat = "yaml";
+  sops.secrets."cloudflared-creds" = {};
 }

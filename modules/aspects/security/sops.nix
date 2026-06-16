f1ec90a15adf33cbfdb20ba15ed6bd36d1c12b09
Yusuf Said Aktan <contact@aktan.org>
2026-06-16 14:30:29 +0300

    security

--- modules/aspects/security/sops.nix
+++ modules/aspects/security/sops.nix
@@ -9,18 +9,18 @@
     ];
     environment.systemPackages = with pkgs; [sops];
     sops.age.sshKeyPaths = ["/home/baba/.ssh/id_ed25519"];
-    sops.defaultSopsFile = ./secrets/secrets.yaml;
+    sops.defaultSopsFile = ../../../secrets/secrets.yaml;
     sops.defaultSopsFormat = "yaml";
     sops.secrets."cloudflared-creds" = {
       format = "json";
-      sopsFile = ./secrets/da38ced1-2335-4195-b6f7-60a99b15a878.json;
+      sopsFile = ../../../secrets/da38ced1-2335-4195-b6f7-60a99b15a878.json;
       key = "";
       mode = "0440";
       group = "keys";
     };
     sops.secrets."kavitaToken" = {
       format = "binary";
-      sopsFile = ./secrets/kavitaToken;
+      sopsFile = ../../../secrets/kavitaToken;
     };
   };
 }
